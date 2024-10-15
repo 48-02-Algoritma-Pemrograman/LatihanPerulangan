244b5a0e91ee9dea3e9566382d8be16b9356e1ad
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-15 23:23:56 +0700

    Add boilerplate

---
+++ TugasSarapan.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class TugasSarapan{
+    public static void main(String[] args){
+        int totalBiaya = 0;
+        int pilihanMenu, uangTersedia;
+        Scanner input = new Scanner(System.in);
+
+        // Pekerjaan anda dimulai dari sini
+        pilihanMenu = input.nextInt();
+        uangTersedia = input.nextInt();
+
+
+        //Pekerjaan anda berakhir disini
+        
+        System.out.println(totalBiaya);
+    }
+}
